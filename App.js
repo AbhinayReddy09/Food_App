b65a0b905edfb44f30265125b29fd06366ee3a22
AbhinayReddyVeerlapally <aveerlap@charlotte.edu>
2025-07-18 15:38:31 +0530

    Start

--- App.js
+++ App.js
@@ -1,2 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./Style.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img 
+                    className="logo" 
+                    src="https://i.pinimg.com/736x/82/be/d4/82bed479344270067e3d2171379949b3.jpg" 
+                    alt="logo" 
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT US</li>
+                    <li>CONTACT US</li>
+                    <li>CART</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const Restaurantcard = () =>{
+    return (
+        <div className="res-card">
+        <img 
+                    className="res-logo" 
+                    alt="logo"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCMEU66bWqrdH31zt3EGGEgeVfDIH6EUZGEQ&s"  
+                />
+            <h3>The Nawabs</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <Restaurantcard/>
+            </div>
+
+        </div>
+    )
+}
+const AppLayout = () => (
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+);
+
+// Mounting the React app to the root div
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # Food
-=======
-# Food Delivery App
->>>>>>> 485577bb7ad3cc63dcedc635ae6c31e711c7ab4f
